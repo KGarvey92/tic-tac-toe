12f3e61722dfc9ec63580e9fb64bcce6ece78c53
KGarvey92 <kgarvey92@gmail.com>
2022-11-17 16:58:30 +0800

    new game form design implemented

--- script.js
+++ script.js
@@ -17,10 +17,30 @@ const gameboard = (() => {
 const gameController = (() => {
 
     const newGame = () => {
-        const btn = document.querySelector('.newGameBtn');
-        btn.addEventListener('click', () => {
+        const newGamebtn = document.querySelector('.newGameBtn');
+        newGamebtn.addEventListener('click', () => {
             document.querySelector('.newGameModal').classList.toggle('hidden');
+        });
+        // event listener to auto select p2 mark.
+        let p1RadioX = document.querySelector('#p1X');
+        p1RadioX.addEventListener('click', () => {
+            document.querySelector('#p2X').removeAttribute("checked", "");
+            document.querySelector('#p2O').setAttribute("checked", "");
+        });
+        let p1RadioO = document.querySelector('#p1O');
+        p1RadioO.addEventListener('click', () => {
+            document.querySelector('#p2O').removeAttribute("checked", "");
+            document.querySelector('#p2X').setAttribute("checked", "");
+        });
+
+        // prevent default form submission behaviour
+        const form = document.querySelector('form');
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
         })
+        
+        // TODO: store and use inputted data (names, marks)
+            // create player objects using data
     }
 
     return {
